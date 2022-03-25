247a0a602dd981394d793347f42939ee5d666941
swu-wpo <1453426719@qq.com>
2022-03-25 22:11:34 +0800

    功能：添加动画库

--- workspace/front/bugu_front/src/router/index.js
+++ workspace/front/bugu_front/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import ProjectLayout from "../views/project/Layout"
+import Login from "../views/login/Login"
+import Register from "../views/login/Login"
 
 Vue.use(VueRouter);
 
@@ -10,15 +12,31 @@ const routes = [
     redirect:'/project'
   },
   {
-      path: '/project',
-      name: 'Project',
-      component: ProjectLayout,
+    path: '/project',
+    name: 'Project',
+    component: ProjectLayout,
+    meta:{
+      index:8
+    }
       // children: [
       //   {
       //     path: '/project/card',
       //     component: () => import('@/views/project/components/Card'),
       //   },
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta:{
+      index:9
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  }
 ];
 
 const router = new VueRouter({
