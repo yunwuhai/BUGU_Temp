52a6f971fb8f8362b2557fedc6bf431865350da2
swu-wpo <1453426719@qq.com>
2022-03-29 02:31:44 +0800

    功能：修改有关配置

--- workspace/front/bugu_front/vue.config.js
+++ workspace/front/bugu_front/vue.config.js
@@ -1,11 +1,38 @@
+const path = require('path')
+function resolve(dir) {
+	return path.join(__dirname, '.', dir)
+}
+
 module.exports = {
   // 基本路径
-  publicPath: "./",
+  // publicPath: "/",
   // 输出文件目录
   outputDir: "dist",
   // webpack-dev-server 相关配置
   // devServer: {
   //   port: 8888,
   // },
+  // 取消生产环境的souceMap，加速生产构建
+  productionSourceMap: false,
   lintOnSave: false, //配置关闭eslint
+  chainWebpack: config => {
+		//配置svg-sprite-loader
+		config.module
+			.rule('svg')
+			.exclude.add(resolve('src/icons'))
+			.end();
+		config.module
+			.rule('svg-sprite-loader')
+			.test(/\.svg$/)
+			.include
+			.add(resolve('./src/icons')) //处理svg目录
+			.end()
+			.use('svg-sprite-loader')
+			.loader('svg-sprite-loader')
+			.options({
+				symbolId: 'icon-[name]'
+			})
+    },
 };
+
+
